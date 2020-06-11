031f056d7282e8e58f184e28f6a96632c4144dfb
Jesse Walling <jessephilipwalling@gmail.com>
2020-06-10 21:41:26 -0700

    secured access to api endpoints

--- routes/courses.js
+++ routes/courses.js
@@ -4,16 +4,6 @@ const Course = require('../models/course');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
-router.get('', (req, res, next) => {
-    Course.getAllCourses((err, data) => {
-        if (err) {
-            res.status(400).json({success: false, err: err});
-        } else {
-            res.status(200).json(data);
-        }
-    });
-});
-
 router.post('/search', (req, res, next) => {
     if (req.body.search.length >=3) {
         Course.searchAllCourses(req.body.search, (err, data) => {
@@ -28,7 +18,7 @@ router.post('/search', (req, res, next) => {
     }
 });
 
-router.get('/get', (req, res, next) => {
+router.get('/get', passport.authenticate('jwt', { session: false }), (req, res, next) => {
     var cid = req.query.cid;
     Course.getCourseById(cid, (err, data) => {
         if (err) {
@@ -37,24 +27,6 @@ router.get('/get', (req, res, next) => {
             res.status(200).json(data);
         }
     });
-});
-
-router.post('/add', (req, res, next) => {
-    Course.addCourse(req.body.name.toUpperCase().replace(/\s/g, ''), req.body.description, (err, data) => {
-        if (err) {
-            res.status(400).json({success: false, msg: "Course already exists!"});
-        } else {
-            res.status(200).json({success: true, msg: "Ok!"});
-        }
-    });
-});
-
-router.post('/remove', (req, res, next) => {
-    res.status(200).json({success: true, msg: "Ok!"});
-});
-
-router.post('/edit', (req, res, next) => {
-    res.status(200).json({success: true, msg: "Ok!"});
 });
 
 module.exports = router;
--- routes/preferences.js
+++ routes/preferences.js
@@ -9,71 +9,70 @@ const COURSE = {"cid": 1, "name": "CSE666", "grade": 4.0};
 const USER_TABLE = {"users": [USER, USER, USER]};
 const COURSE_TABLE = {"courses": [COURSE, COURSE, COURSE]};
 
-router.get('', (req, res, next) => {
-    let cid = req.query.cid;
-    let uid = req.query.uid;
-    if (cid && uid) {
-        res.status(200).json(COURSE);
-    } else if (cid) {
-        Preference.getUsersInCourse(cid, (err, data) => {
+router.get('', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    Preference.getUsersInCourse(req.query.cid, (err, data) => {
+        if (err) {
+            res.status(400).json({success: false, err: err});
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+router.post('/userCourses', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    if (req.user.uid == req.body.uid) {
+        Preference.getCoursesByUser(req.body.uid, (err, data) => {
             if (err) {
-                res.status(400).json({success: false, err: err});
+                res.status(400).json({success: false, msg: "Unable to retrieve your listed courses"});
             } else {
-                res.status(200).json(data);
+                res.status(200).json({success: true, msg: data});
             }
         });
-    } else if (uid) {
-        res.status(200).json(COURSE_TABLE);
-    } else if (Object.keys(req.query).length === 0) {
-        res.status(200).json({success: true, msg: "Ok! You must be an admin!"});
     } else {
-        res.status(400).json({success: false, msg: "Unknow parameter!"});
+        res.status(401).json({success: false, msg: "You are not allowed to access data from another user."});
     }
-    
 });
 
-router.post('/userCourses', (req, res, next) => {
-    Preference.getCoursesByUser(req.body.uid, (err, data) => {
-        if (err) {
-            res.status(400).json({success: false, msg: "Unable to retrieve your listed courses"});
-        } else {
-            res.status(200).json({success: true, msg: data});
-        }
-    });
+router.post('/add', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    if (req.user.uid == req.body.uid) {
+        Preference.addUserToCourse(req.body.uid, req.body.cid, req.body.minGrade, (err, data) => {
+            if (err) {
+                res.status(400).json({success: false, msg: "You are already in this course!"});
+            } else {
+                res.status(200).json({success: true, msg: "Ok!"});
+            }
+        });
+    } else {
+        res.status(401).json({success: false, msg: "You are not allowed to modify data for another user."});        
+    }
 });
 
-router.post('/add', (req, res, next) => {
-    Preference.addUserToCourse(req.body.uid, req.body.cid, req.body.minGrade, (err, data) => {
-        if (err) {
-            res.status(400).json({success: false, msg: "You are already in this course!"});
-        } else {
-            res.status(200).json({success: true, msg: "Ok!"});
-        }
-    });
+router.post('/check', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    if (req.user.uid == req.body.uid) {
+        Preference.isUserInCourse(req.body.uid, req.body.cid, (err, data) => {
+	    if (err) {
+	        res.status(400).json({success: false, msg: "Error checking if user is in course"});
+	    } else {
+	        res.status(200).json({success: true, msg: data});
+	    }
+        });
+    } else {
+        res.status(401).json({success: false, msg: "You are not allowed to access data from another user."});        
+    }
 });
 
-router.post('/check', (req, res, next) => {
-    Preference.isUserInCourse(req.body.uid, req.body.cid, (err, data) => {
-	if (err) {
-	    res.status(400).json({success: false, msg: "Error checking if user is in course"});
-	} else {
-	    res.status(200).json({success: true, msg: data});
-	}
-    });
-});
-
-router.post('/remove', (req, res, next) => {
-    Preference.removeUserFromCourse(req.body.uid, req.body.cid, (err, data) => {
-	if (err) {
-	    res.status(400).json({success: false, msg: "Failed to delete user entry from course!"});
-	} else {
-	    res.status(200).json({success: true, msg: req.body.cid});
-	}
-    });
-});
-
-router.post('/edit', (req, res, next) => {
-    res.status(200).json({success: true, msg: "Ok!"});
+router.post('/remove', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    if (req.user.uid == req.body.uid) {
+        Preference.removeUserFromCourse(req.body.uid, req.body.cid, (err, data) => {
+	    if (err) {
+	        res.status(400).json({success: false, msg: "Failed to delete user entry from course!"});
+	    } else {
+	        res.status(200).json({success: true, msg: req.body.cid});
+	    }
+        });
+    } else {
+        res.status(401).json({success: false, msg: "You are not allowed to modify data for another user."});
+    }
 });
 
 module.exports = router;
